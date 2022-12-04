4b7340116b03f57dcf7a35486e489557434b3cd8
bachpx195 <bachpx195@gmail.com>
2022-12-04 20:32:47 +0700

    first commit

---
+++ contracts/Hello_world.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Hello {
+    string public hello_solidity = "Hello world";
+
+    // fixed-size types
+    bool inEnable; // true, false
+    uint number;    // 1,2,3...
+    address sender; // 0x...2323
+    bytes32 data; // cho data dạng hằng số để tiết kiệm
+
+    // variable-size types
+    string name; // "hello"
+    bytes data_no_lenght;
+    uint[] amounts; // [1,2,3,4] cung kieu du lieu trong 1 array
+    mapping (address => bool) whitelist; // giong voi object trong js, 0x...1213 --> true 
+
+    // user define 
+    struct User {
+        uint id;
+        string name;
+        bool isFriend;
+    }
+
+    enum Color {
+        red,
+        green,
+        blue
+    }
+
+    // built in
+    address msg_sender = msg.sender;
+    uint256 msg_value = msg.value;
+
+    constructor (uint _a) public {
+        number = _a;
+    }
+
+    function getNumber() external view returns(uint) {
+        return number;
+    }
+
+    function setNumber(uint _value) external {
+        number = _value;
+    }
+
+    // visibility
+    // private, chỉ gọi ở bên trong contract
+    // internal, chỉ gọi đc từ contract kế thừa
+    // external, chỉ đc gọi ở bên ngoài
+    // public, gọi ở mọi nơi
+    // đối với variable mặc định là private
+
+
+    // controll structures
+    
+} 
